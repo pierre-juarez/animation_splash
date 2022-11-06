e4c9a97eff7f659d44a19735d261ea11ceeab535
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-05 22:57:07 -0500

    Initial Commit

---
+++ splashAnimation/splashAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  splashAnimationApp.swift
+//  splashAnimation
+//
+//  Created by Tech Fitco on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct splashAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
